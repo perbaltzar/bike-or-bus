c766e29a9b8b0a39961a101185a0d12dd926f444
Per Baltzar <per.baltzar@gmail.com>
2019-10-02 19:59:15 +0200

    Button component in s-c

---
+++ src/components/Button.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ButtonStyled = styled.button`
+  width: 150px;
+  height: 50px;
+  background: ${({ theme }) => theme.darkPurple};
+  border: 5px solid ${({ theme }) => theme.text};
+  border-radius: 10px;
+  margin-top: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.text};
+  font-family: 'Baloo Chettan', cursive;
+  font-size: 1rem;
+
+  :hovercursor: pointer;
+  border: 5px solid ${({ theme }) => theme.darkPurple};
+  background: ${({ theme }) => theme.text};
+  color: ${({ theme }) => theme.darkPurple};
+`;
+
+const Button = ({ onClick, text }) => {
+  return <ButtonStyled onClick={onClick}>{text}</ButtonStyled>;
+};
+
+export default Button;
