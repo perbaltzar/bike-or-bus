724557e891945b4ddaff59f7eaaeb0f69c3f6c10
Per Baltzar <per.baltzar@gmail.com>
2019-10-04 18:01:32 +0200

    added all to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,17 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 import Headline from './Headline';
 import Button from './Button';
+import Results from './Results';
+import Weather from './Weather';
+
 const StyledMain = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-const Main = props => {
+const WeatherContainer = styled.div`
+display: flex;
+justify-content: space-between;
+`;
+
+const Main = ({ settings, setSettings }) => {
+  console.log(settings);
   return (
     <StyledMain>
       <Headline destination={'Yrgo'} />
+      <Results vehicle="bus" reason={'Too Cold'} />
       <Button text={"Let's Check"} />
+      <WeatherContainer>  
+        <Weather headline="To" weather={'rain'} temperature={'3'} />
+        <Weather headline="Home" weather={'snow'} temperature={'3'} />
+      </WeatherContainer>
     </StyledMain>
   );
 };
