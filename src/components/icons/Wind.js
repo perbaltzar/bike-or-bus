3e5e29e6073b96233588df1d0ca29cb69b7a2de8
Per Baltzar <per.baltzar@gmail.com>
2019-10-04 17:58:19 +0200

    added icons components in /icons

---
+++ src/components/icons/Wind.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Wind = () => {
+  return (
+    <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M7.1 9.1A4 4 0 008 17h17a4 4 0 001-7.9v-.6a4.5 4.5 0 00-7.6-3.2A6 6 0 007 9zM27 8.4a5 5 0 01-2 9.6H8a5 5 0 01-2-9.6V8a7 7 0 0112.6-4.2A5.5 5.5 0 0127 8.4zM30 23a4 4 0 00-8 0h1a3 3 0 113 3H3v1h23a4 4 0 004-4zm-9-1a3 3 0 00-6 0h1a2 2 0 114 0 2 2 0 01-2 2H6v1h12a3 3 0 003-3zm4 8a2 2 0 01-4 0h1c0 .6.4 1 1 1s1-.4 1-1-.4-1-1-1H9v-1h14a2 2 0 012 2z"
+        fill="#fff3db"
+        fill-rule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default Wind;
---
+++ src/components/icons/index.js
@@ -0,0 +1,7 @@
+export { default as Bus } from './Bus';
+export { default as Cloud } from './Cloud';
+export { default as Rain } from './Rain';
+export { default as Settings } from './Settings';
+export { default as Snow } from './Snow';
+export { default as Sun } from './Sun';
+export { default as Wind } from './Wind';
