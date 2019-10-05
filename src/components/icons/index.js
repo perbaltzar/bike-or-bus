4cf476d2f90a7dac319f6c69b47fccfe98ee0cbc
Per Baltzar <per.baltzar@gmail.com>
2019-10-05 13:49:26 +0200

    added exclamation icon

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,6 +1,7 @@
 export { default as Bike } from './Bike';
 export { default as Bus } from './Bus';
 export { default as Cloud } from './Cloud';
+export { default as Exclamation } from './Exclamation';
 export { default as Rain } from './Rain';
 export { default as Settings } from './Settings';
 export { default as Snow } from './Snow';
