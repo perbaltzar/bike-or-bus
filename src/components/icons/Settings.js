763f43fd1f27d9d79d27ce3a86481d6c29f9d3bb
Per Baltzar <per.baltzar@gmail.com>
2019-10-05 11:50:31 +0200

    made icons resizable

--- src/components/icons/Settings.js
+++ src/components/icons/Settings.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 
 const SettingsStyled = styled.div`
   position: absolute;
-  bottom: 20px;
-  left: 20px;
+  bottom: 5px;
+  left: 5px;
   z-index: 100;
 `;
 const Settings = ({ settings, onClick }) => {
