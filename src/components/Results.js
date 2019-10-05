ea212d94fb0102a5b19f17ec37e87275ef0de101
Per Baltzar <per.baltzar@gmail.com>
2019-10-05 13:33:21 +0200

    new layout in Results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Bus } from './icons';
+import { Bus, Bike } from './icons';
 
 const StyledResults = styled.div`
   width: 65vw;
@@ -13,18 +13,30 @@ const StyledResults = styled.div`
   box-shadow: 7px 7px 1px ${({ theme }) => theme.darkPurple};
   border-radius: 30px;
   margin-top: 20px;
-
+  p {
+    margin-top: 10px;
+  }
   svg {
     height: auto;
     width: 60%;
   }
 `;
 
-const Results = ({ vehicle, reason }) => {
+const Results = ({ vehicle, reason, showResults }) => {
   return (
     <StyledResults>
-      {vehicle === 'bus' && <Bus />}
-      <p className="reason">{reason}</p>
+      {showResults && vehicle === 'bus' && (
+        <>
+          <Bus />
+          <p className="reason">{reason}</p>
+        </>
+      )}
+      {showResults && vehicle === 'bike' && (
+        <>
+          <Bike />
+          <p className="reason">{reason}</p>
+        </>
+      )}
     </StyledResults>
   );
 };
