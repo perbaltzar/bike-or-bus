78d30b06558383b2cfbbf0f19e4d9529e323e529
Per Baltzar <per.baltzar@gmail.com>
2019-10-06 17:15:19 +0200

    Settings component styled

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledSettings = styled.div``;
+import Button from './Button';
+const StyledSettings = styled.div`
+  height: 90vh;
+  width: calc(100vw - 20px);
+  position: absolute;
+  margin: 10px;
+  background-image: ${({ theme }) => theme.background};
+  z-index: 110;
+  border-radius: 15px;
+  border: 7px solid ${({ theme }) => theme.text};
+  color: ${({ theme }) => theme.text};
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  div {
+    width: 100%;
+    padding: 5px;
+  }
 
-const Settings = props => {
-  return <StyledSettings></StyledSettings>
+  h4 {
+    letter-spacing: 2px;
+    font-size: 30px;
+    text-shadow: 2px 3px 1px ${({ theme }) => theme.darkPurple};
+  }
+  form {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Settings = ({ settings }) => {
+  return (
+    <StyledSettings>
+      <h4>Settings</h4>
+      <div>
+        <p>Where do you want to go?</p>
+        <form>
+          <input type="text" />
+          <Button text="Set" />
+        </form>
+      </div>
+    </StyledSettings>
+  );
 };
 
 export default Settings;
