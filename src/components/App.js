e9d4b3354da0ed24fcccc34756328dc273ff4983
Per Baltzar <per.baltzar@gmail.com>
2019-10-06 17:14:13 +0200

    added toggle func in app

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,35 @@ import Main from './Main';
 import GlobalStyles from '../styles/GlobalStyles';
 import Theme from '../styles/Theme';
 import Settings from './Settings';
-import { Settings as SettingsIcon } from './icons';
+import Info from './Info';
+import { Settings as SettingsIcon, Exclamation } from './icons';
 import { SettingsProvider } from './Context';
 
 const App = props => {
   const [settings, setSettings] = useState(false);
+  const [info, setInfo] = useState(false);
+
+  // Closing info while opening settings
+  const toggleSettings = () => {
+    setSettings(!settings);
+    setInfo(false);
+  };
+
+  //
+  const toggleInfo = () => {
+    setInfo(!info);
+    setSettings(false);
+  };
+
   return (
     <>
       <ThemeProvider theme={Theme}>
         <GlobalStyles />
         <SettingsProvider>
           <Layout>
-            <SettingsIcon settings={settings} onClick={() => setSettings(!settings)} />
+            <SettingsIcon settings={settings} onClick={toggleSettings} />
+            {/* <Exclamation info={info} onClick={toggleInfo} />
+            {info && <Info />} */}
             {settings && <Settings />}
             <Main />
           </Layout>
